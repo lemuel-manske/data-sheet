4cc64cda9dfd277209e0f6fb6923e7fe04fbe2f5
Lemuel Kauê Manske de Liz <lemuelkaue@gmail.com>
2024-07-03 17:37:24 -0300

    feat: added custom error message for not found purchase

--- api/src/main/java/application/controllers/PurchaseNotFound.java
+++ api/src/main/java/application/controllers/PurchaseNotFound.java
@@ -3,5 +3,12 @@ package application.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class PurchaseNotFound extends RuntimeException { }
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = PurchaseNotFound.PURCHASE_NOT_FOUND)
+public class PurchaseNotFound extends RuntimeException {
+
+    public static final String PURCHASE_NOT_FOUND = "Purchase not found";
+
+    public PurchaseNotFound() {
+        super(PurchaseNotFound.PURCHASE_NOT_FOUND);
+    }
+}
