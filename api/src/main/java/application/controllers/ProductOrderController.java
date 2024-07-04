4d38197e5085700705120faabe6bcaeb3a06f9d8
Lemuel Kauê Manske de Liz <lemuelkaue@gmail.com>
2024-07-04 19:31:39 -0300

    style: removed not used import

--- api/src/main/java/application/controllers/ProductOrderController.java
+++ api/src/main/java/application/controllers/ProductOrderController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class ProductOrderController implements ProductOrderResource {
