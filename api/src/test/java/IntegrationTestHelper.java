1ccef95ffff75824a3c390dc2dfec7721923cff0
Lemuel Kauê Manske de Liz <lemuelkaue@gmail.com>
2024-07-04 10:33:33 -0300

    test: changed `TestRestTemplate` to `MockMvc`

---
+++ api/src/test/java/IntegrationTestHelper.java
@@ -0,0 +1,19 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+public final class IntegrationTestHelper {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static <T> T readAs(Class<T> objectToRead, String jsonContent) throws JsonProcessingException {
+        return mapper.reader().forType(objectToRead).readValue(jsonContent);
+    }
+
+    public static <T> String writeAsString(T objectToRead) throws JsonProcessingException {
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+
+        return ow.writeValueAsString(objectToRead);
+    }
+
+}
--- api/src/test/java/PurchaseResourceIntegrationTest.java
+++ api/src/test/java/PurchaseResourceIntegrationTest.java
@@ -1,58 +1,74 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.TestPropertySource;
-
-import java.net.URI;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.TestPropertySource;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import application.Application;
 import application.purchase.PurchaseDto;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+@AutoConfigureMockMvc
 @TestPropertySource(locations = { "classpath:application-integrationtest.properties" })
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = { Application.class })
 class PurchaseResourceIntegrationTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate template;
+    private MockMvc mockMvc;
 
-    private static final String PURCHASE_ENDPOINT = "/purchase";
+    private PurchaseFactory factory;
+
+    @BeforeEach
+    void setUp() {
+        factory = new PurchaseFactory();
+    }
 
     @Test
-    void whenPurchaseIsCreated_ThenGetIt() {
-        PurchaseDto actualCreatedPurchase = doPostValidPurchase();
+    void whenPurchaseIsCreated_ThenGetIt() throws Exception {
+        MockHttpServletResponse postResponse = doPostValidPurchase(factory.validBananaPurchase())
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse();
 
-        PurchaseDto foundPurchase = doGetPurchase(actualCreatedPurchase.getId());
+        PurchaseDto actualCreatedPurchase =
+                IntegrationTestHelper.readAs(PurchaseDto.class, postResponse.getContentAsString());
 
-        assertThat(actualCreatedPurchase).isEqualTo(foundPurchase);
+        MockHttpServletResponse getResponse = doGetPurchase(actualCreatedPurchase.getId())
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+
+        PurchaseDto gotPurchase =
+                IntegrationTestHelper.readAs(PurchaseDto.class, getResponse.getContentAsString());
+
+        assertThat(actualCreatedPurchase).isEqualTo(gotPurchase);
     }
 
-    private PurchaseDto doGetPurchase(String purchaseId) {
-        String endpoint = PURCHASE_ENDPOINT + "/" + purchaseId;
+    private ResultActions doGetPurchase(String purchaseId) throws Exception {
+        MockHttpServletRequestBuilder getRequest = get("/purchase/" + purchaseId);
 
-        return template.getForObject(endpoint, PurchaseDto.class);
+        return mockMvc.perform(getRequest);
     }
 
-    private PurchaseDto doPostValidPurchase() {
-        PurchaseRequest purchase = new PurchaseRequest();
+    private ResultActions doPostValidPurchase(PurchaseDto aPurchase) throws Exception {
+        String json = IntegrationTestHelper.writeAsString(aPurchase);
 
-        purchase.addOrder(
-                "Banana",
-                "2.99", "BRL",
-                "1.50", "KILOGRAM");
+        MockHttpServletRequestBuilder postRequest = post("/purchase")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
 
-        return template.postForObject(server(), purchase.getRequest(), PurchaseDto.class);
+        return mockMvc.perform(postRequest);
     }
-
-    private URI server() {
-        return URI.create(String.format("http://localhost:%d%s", port, PURCHASE_ENDPOINT));
-    }
-
 }
